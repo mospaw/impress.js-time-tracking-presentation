a8277a0bdb3cb3c3ddc8e572c1c111f9b1da235b
Chris Mospaw <cmospaw@crowdfavorite.com>
2014-06-26 11:11:40 -0600

    first draft

--- index.php
+++ index.php
@@ -44,7 +44,8 @@
 
 	<p>Memory of what you do day-to-day fades quickly. If it's noted in a coherent way, it’s a lot easier to bill for.</p>
 
-	<p>If there’s ever a question about what was done, there is an authoritative record of what was done, for whom, and when. </p>
+	<p><i>Doing a little extra work allows me to stay lazy.</i> My computer rememebrs the details.</p>
+
 <?php slide_close(); ?>
 
 
@@ -75,19 +76,20 @@
 
 	<p><b>Simple Time Track</b><p>
 
-	<p>Not overburdened with features you won’t need.</p>
-	<p>Supports multiple named stopwatches</p>
-	<p>Stopwatches start and stop independently and can run simultaneously.</p>
-	<p>Records time even after restarting the browser or rebooting the computer.</p>
+	<ul>
+		<li>Not overburdened with features you won’t need.</li>
+		<li>Supports multiple named stopwatches</li>
+		<li>Stopwatches start and stop independently and can run simultaneously.</li>
+		<li>Records time even after restarting the browser or rebooting the computer.</li>
+	</ul>
 
 <?php slide_close(); ?>
 
 
 <?php slide_open(); ?>
-	<h1 id="the-spreadsheet">The spreadsheet</h1>
+	<h1 id="the-spreadsheet">Spreadsheet Columns</h1>
 
 	<img class="hero" src="spreadsheet.png" width="325">
-	<p>What the columns mean...<p>
 
 	<h2 id="billed">Billed</h2>
 
@@ -101,30 +103,54 @@
 
 	<p>The amount to be billed, in quarter-hour increments.</p>
 
+	<h2 id="hours">Descriptions</h2>
+
+	<p>More on this later...</p>
+
+
 <?php slide_close(); ?>
 
 
 <?php slide_open(); ?>
-	<h1 id="the-spreadsheet2">The spreadsheet continued</h1>
+	<h1 id="the-spreadsheet5">More columns</h1>
+
+	<img class="hero" src="spreadsheet.png" width="300">
+
+	<h2 id="task">Task</h2>
+
+	<p>A place to cross-reference the task or tasks for this line item<ul>
+	<li> Generally a URL, but sometimes just a description</li>
+	<li> Cross-reference in the future</li>
+	</ul></p>
+
+	<h2 id="notes">Notes</h2>
+
+	<p>Any quick notes that might be needed can be put in an additional notes column. Lengthier Notes should be taken in the note taking editor and linked here.</p>
+
+
+<?php slide_close(); ?>
+
+<?php slide_open(); ?>
+	<h1 id="the-spreadsheet2">So what about those descriptions?</h1>
 
 	<img class="hero" src="spreadsheet.png" width="350">
 	<h2 id="description">Description</h2>
 
-	<p>In plain words, tell what you did. Be descriptive. Heed our <a href="https://crowdfave.org/policies/billing-hours/">“Line Item Language”</a>.</p>
+	<p>In plain words, tell what you did. Be descriptive. Heed our <b>“Line Item Language”</b>.</p>
 	
 	<br>
 
 	<h2 id="justification">Bigger is better</h2>
 
-	<p>A description that goes into detail about steps taken is a lot easier for the client to understand. It's also less likely to get questioned
+	<p>Descriptions that <b>go into detail</b> about steps taken is a lot <b>easier for the client to understand</b>. Line items are also less likely to get questioned
 		if specifics are mentioned.</p>
 
-	<p>A client that understands what was done is also a happy client.</p>
+	<p><i>A client that understands what was done is also a happy client.</i></p>
 <?php slide_close(); ?>
 
 
 <?php slide_open(); ?>
-	<h1 id="the-spreadsheet3">Justifying descriptions</h1>
+	<h1 id="the-spreadsheet3">Justifying lenghty descriptions</h1>
 
 	<p>Which one of these line items better justifies 3 hours of billing?<p>
 
@@ -146,52 +172,52 @@
 
 	<img class="hero" src="don-hewitt.jpg" width="325">
 
-	<p>Don Hewitt created the TV show <em>60 Minutes.</em> His line to  the reporters was <b>Tell me a story</b>. Tell the client a story. Be specific. Show the entire effort.</p>
+	<p>Don Hewitt created the TV show <em>60 Minutes.</em> His command to reporters was <b>"Tell me a story"</b>. Tell the client a story.</p>
+
+<ul>
+	<li>Be specific. </li>
+	<li>Be truthful. </li>
+	<li>Describe the entire effort.</li>
+</ul>
 
 	<p>Long descriptions can be broken into two or more billing lines. <i>Simply split the time and text appropriately.</i></p>
 
-	<p>More billing lines with smaller times next to them is better than large amounts of time billed to one line item. </p>
+<?php slide_close(); ?>
+
+
+<?php slide_open(); ?>
+	<h1 id="the-spreadsheet4">Good Descriptions equal<br> Happy Clients</h1>
+
+	<img class="hero" src="happy-clients.jpg" width="375">
+
+	<p>Our experience has shown that:</p>
+
+	<p><b>More billing line items with smaller times next to them ...</b></p>
+	<p>... are better than ...</p>
+	<p>... <i>large amounts of time billed to one line item.</i></p>
 
 	<p>Put yourself in the client’s shoes, tell a good story, and use your own judgment. </p>
-<?php slide_close(); ?>
-
-
-<?php slide_open(); ?>
-	<h1 id="the-spreadsheet5">More columns</h1>
-
-	<img class="hero" src="spreadsheet.png" width="350">
-
-	<h2 id="task">Task</h2>
-
-	<p>A place to cross-reference the task or tasks<br>
-	&nbsp; &nbsp; - Generally a URL, but sometimes just a description<br>
-	&nbsp; &nbsp; - Great cross-reference in the future
-
-	<h2 id="notes">Notes</h2>
-
-	<p>Any quick notes that might be needed can be put in an additional notes column. Lengthier Notes should be taken in the note taking editor and linked here.</p>
-
 
 <?php slide_close(); ?>
 
-
 <?php slide_open(); ?>
 
-	<h1 id="notes">Speaking of Notes</h1>
+	<h1 id="notes">Worth Noting</h1>
 
 	<img class="hero" src="capsule.png" width="300">
 
-	<p>The free-form human-friendly stuff that should be recorded for every non-trivial task and many of the trivial ones as well.</p>
+	<p><b>Notes:</b> That free-form human-friendly stuff that should be recorded for every single task.</p>
 
 	<h2>Forms that notes can take</h2>
 
-	<p> - Plain language description of what was done<br>
-		- Line items documenting a specific process<br>
-		- End user or technial documentation<br>
-		- Any combintation of these
-	</p>
+	<ul>
+	<li> Plain language description of what was done</li>
+	<li> Line items documenting a specific process</li>
+	<li> End user or technial documentation</li>
+	<li> Any combintation of these</li>
+	</ul>
 
-	<p>What’s most important is that any fleeting thoughts that are not part of the code get captured and organized somehow.</p>
+	<p class="small">What’s most important is that any fleeting thoughts that are not part of the code somehow get captured and organized.</p>
 
 <?php slide_close(); ?>
 
@@ -203,12 +229,13 @@
 
 	<p>Answer: <b>Everything</b></p>
 
-	<p>If a task takes minute or two it can get rolled into “Billing, email, task management”</p>
+<ul>
+	<li>If a task takes minute or two it can get rolled into “Billing, email, task management”</li>
 	
-	<p>Anything that takes more than a few minutes gets billed.</p>
+	<li>Anything that takes more than a few minutes gets billed.</li>
 
-	<p>If in doubt, <b>bill it</b> instead of rolling it into “Billing, email, task management”, which should generally be around 30 minutes a day or less.</p>
-
+	<li>If in doubt, <b>bill it</b> instead of rolling it into “Billing, email, task management”, which should generally be around 30 minutes a day or less.</li>
+</ul>
 	<p class="small"><b>Note:</b> It’s perfectly fine to roll two or three minor tasks into one billing line if they’re all related to one client.</p>
 
 <?php slide_close(); ?>
@@ -217,15 +244,17 @@
 <?php slide_open(); ?>
 	<h1 id="what-to-track">Why track <i>everything</i>?</h1>
 
-	<p>It’s our responsibility to provide an accurate <em>and complete</em> record of what we do.<p>
+	<img class="hero" src="tracking2.jpg" width="300">
 
-	<p>If something needs to be adjusted, it’s up to managers to make the adjustment or at least bring it to your attention. By billing everything &mdash; even internal or “overhead” &mdash; you are providing the company critical information. </p>
+	<p>We should provide an accurate <em>and complete</em> record of what we do.<p>
+
+	<p>If something needs to be adjusted, <b>it’s up to managers to make the adjustment or at least bring it to your attention.</b></p>
 
 	<h2>Bottom Line</h2>
 
-	<p>If you’re going to err, err on the side of billing too much and providing too much information.</p>
+	<p>If you’re going to err, <b>err on the side of billing too much</b> and providing too much information.</p>
 
-	<p>It’s far easier to remove unneeded information and adjust a billing line than it is to read someone’s mind.</p>
+	<p class="small">It’s far easier to remove unneeded information and adjust a billing line than it is to read someone’s mind.</p>
 
 <?php slide_close(); ?>
 
@@ -233,11 +262,27 @@
 <?php slide_open(); ?>
 	<h1 id="workflow">Workflow</h1>
 
-	<p>The workflow is simple and deliberate, designed to be easy to become a habit and to minimize any extra work. Entering time into the billing system is a snap, since it’s basically copy and paste. There are a few items things that occur on specific days, but generally each item is ongoing.</p>
+	<img class="hero" src="complex-workflow.jpg" width="400">
 
-	<h2 id="monday-morning-or-first-day-of-your-week">Monday morning (or first day of your week)</h2>
+	<ul>
+	<li>This workflow is simple and deliberate</li>
+	<li>Designed to become a habit and to minimize extra work</li>
+	<li>Entering time into the billing system is a snap, since it’s basically copying and pasting descriptions
+	<li>Finding out what you did in the past is a search away
+	</ul>
 
-	<p>Open up this week’s spreadsheet and “File &gt;&gt; Make a copy…” to save next week’s spreadsheet. Edit the tabs to the appropriate dates on the new spreadsheet.</p>
+<?php slide_close(); ?>
+
+<?php slide_open(); ?>
+	<h1 id="workflow">Monday Morning</h1>
+
+	<h2 id="monday-morning-or-first-day-of-your-week">(or first day of your week)</h2>
+
+	<img class="hero" src="spreadsheet.png" width="325">
+<ul>
+	<li>Open up this week’s spreadsheet and <b>“File &gt;&gt; Make a copy…”</b> and rename to save next week’s spreadsheet</li>
+	<li>Edit the tabs to the appropriate dates on the new spreadsheet.</li>
+</ul>
 
 	<p>That gives you a fresh and empty spreadsheet for next week and lets you get started billing this week’s activities.</p>
 
@@ -245,54 +290,57 @@
 
 
 <?php slide_open(); ?>
-	<h1 id="workflow2">Workflow</h1>
+	<h1 id="workflow2">Starting a new task</h1>
 
-	<h2 id="starting-a-new-task">Starting a new task</h2>
+	<div class="hero">
+	<img  src="simple-time-track.png" width="250"><br>
+	<img  src="spreadsheet.png" width="250"><br>
+	<img  src="capsule.png" width="250">
+	</div>
 
-	<p>When starting a new task, I click on the Simple Time Track icon and add a new stopwatch with a name for that task. I then create a line item on today’s spreadsheet with the client name and paste the URL to the task (Sifter, TasksPro, GitHub issue, etc.) into the task column. I also usually put in a quick summary under “Description”.</p>
-
-	<p>This is also a good time to fire up a new note (or edit a previous one as appropriate) in Capsule or whatever you’re using to take notes.</p>
-
+<ul>
+	<li>Add a new stopwatch with task and client info to Simple Time Track
+	<li>Create a line item on today’s spreadsheet with the client name and paste the URL to the task (Sifter, TasksPro, GitHub issue, etc.) into the task column</p>
+	<li>Fire up a new note or edit a previous one inyour note taker</p>
+</ul>
 <?php slide_close(); ?>
-
-
-<?php slide_open(); ?>
-	<h1 id="switching-tasks">Switching tasks</h1>
-
-	<img class="hero" src="simple-time-track.png" width="375">
-
-	<p>When switching tasks, I’ve gotten into the habit of also switching the stopwatch or creating a new one as necessary.</p>
-
-	<p>By doing this, you’re always correctly tracking time. You can always split time out or combine it later if necessary. 
-
-	<p>But no matter what, <b>always track your time</b>.</p>
-
-<?php slide_close(); ?>
-
-
 
 <?php slide_open(); ?>
 	<h1 id="tldr">TL;DR</h1>
 
-	<img class="hero" src="cool-story.jpg" width="325">
+	<img class="hero" src="cool-story.jpg" width="350">
 
-	<p>If you don’t bill, the company doesn’t get paid. If the company doesn’t get paid, you get don’t paid. Do your billing and get paid. Tracking time is actually easy. Bonus: if you track everything you do accurately, explaining what you did later on is a lot easier. </p>
+	<p>If you don’t bill, the company doesn’t get paid. <b>If the company doesn’t get paid, you get don’t paid.</b></p>
+	<p>Do your billing and get paid. <b>Tracking time actually becomes easy.</b></p>
+	<p><b>Bonus:</b> If you track everything you do accurately, explaining what you did later on is a lot easier. </p>
 
-	<p>I call all of this effort “Love Notes to Future Chris”. Feel free to insert your own name where “Chris” is in the previous sentence.</p>
+	<p>I call all of this effort <b>“Love Notes to Future Chris”</b>.</p>
 <?php slide_close(); ?>
 
 
 <?php slide_open(); ?>
 	<h1 id="conclusion">Conclusion</h1>
 
-	<img class="hero" src="aliens.jpg" width="325">
+	<img class="hero" src="aliens.jpg" width="350">
 
-	<p>The more successful you are at tracking time completely and accurately as well as billing that time, the more successful your projects, and therefore the company, will be. You will also have a comprehensive record of everything you do, which makes those “down the road” questions much easier to answer.</p>
+	<p>Successful and thorough time tracking and billing leads to:</p>
 
-<?php slide_open(); ?>
+	<ul>
+		<li>Successful projects</li>
+		<li>A profitable company</li>
+		<li>A comprehensive record of everything you do, which makes those “down the road” questions much easier to answer.</li>
+	</ul>
+	<img class="hero" src="chris.jpg" width="125">
 
+<br>
+	<p>This will be covered in detail on an intranet page. Feel free to ping <b>Chris Mospaw</b> with questions.</p>
 
+<?php slide_close(); ?>
+<?php slide_open('step'); ?>
+<img src="thats-All-folks.jpg" width="900">
+<?php slide_close(); ?>
 
+<?php slide_open('step'); ?>
 
 
 <script src="js/impress.js"></script>
@@ -302,12 +350,16 @@
 
 <?php 
 
-function slide_open() {
+function slide_open($override_classes = null) {
 	static $position;
 	static $slide;
+	$classes = 'step slide';
+	if (null !== $override_classes) {
+		$classes = $override_classes;
+	}
 	$slide++;
 	?>
-	    <div id="slide-<?php echo $slide; ?>" class="step slide" data-x="0" data-y="<?php echo $position; ?>">
+	    <div id="slide-<?php echo $slide; ?>" class="<?php echo $classes; ?>" data-x="0" data-y="<?php echo $position; ?>">
 	<?php
 	$position += 1000;
 }
